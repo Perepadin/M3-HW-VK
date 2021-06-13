23573af9e6e0473b293405a7b24d61f68097d26f
Perepadin <82140378+Perepadin@users.noreply.github.com>
2021-06-13 19:56:59 +0300

    v2.1

--- src/main/java/ru/netology/domain/CommentsInfo.java
+++ src/main/java/ru/netology/domain/CommentsInfo.java
@@ -3,18 +3,18 @@ package ru.netology.domain;
 public class CommentsInfo {
     private int countComments;          // общий счетчик комментариев
     private int ownerCommentId;         // ID комментария (возможно лишний показатель - нет в документации)
-    private String CommentsBody;        // тело комментария (возможно лишний показатель - нет в документации)
+    private String commentsBody;        // тело комментария (возможно лишний показатель - нет в документации)
     private int countCommentIdLikes;    // подсчет likes на комментарии (возможно лишний показатель - нет в документации)
     private String commentsOwnerName;   // имя автора комментария (возможно лишний показатель - нет в документации)
     private int commentsOwnerImageUrl;  // изображение автора комментария (возможно лишний показатель - нет в документации)
-    private boolean canСomment;         // может ли текущий пользователь оставить комментарий
-    private boolean userСomment;        // наличие комментария от текущего пользователя
+    private boolean canComment;         // может ли текущий пользователь оставить комментарий
+    private boolean userComment;        // наличие комментария от текущего пользователя
 
 
-    private boolean groupsCanСomment;   // v2 поле groups_can_post - информация о том,могут ли сообщества комментировать запись;
+    private boolean groupCanComment;    // v2 поле groups_can_post - информация о том,могут ли сообщества комментировать запись;
                                         // рассматривал функцию со стороны пользователя, забыл про группы.
-    private boolean canCloseСomment;    // v2 поле can_close - может ли текущий пользователь закрыть комментарии к записи;
-                                        //не учел уровень доступа к функции.
-    private boolean canOpenСomment;     // v2 поле can_open -  может ли текущий пользователь открыть комментарии к записи.
-                                        //не учел уровень доступа к функции.
+    private boolean canCloseComment;    // v2 поле can_close - может ли текущий пользователь закрыть комментарии к записи;
+                                        // не учел уровень доступа к функции.
+    private boolean canOpenComment;     // v2 поле can_open -  может ли текущий пользователь открыть комментарии к записи.
+                                        // не учел уровень доступа к функции.
 }
--- src/main/java/ru/netology/domain/GeoInfo.java
+++ src/main/java/ru/netology/domain/GeoInfo.java
@@ -4,5 +4,5 @@ public class GeoInfo {  // класс введен в v2
 
     private String type; // v2 тип места;
     private String coordinates; // v2 координаты места;
-    private String place; // v2 описание места;
+    private String place; // v2 описание места (если оно добавлено).
 }
